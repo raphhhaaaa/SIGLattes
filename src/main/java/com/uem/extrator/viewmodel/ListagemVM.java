b16713ad33739f03e8b4f9aba49f7ab03ed2e7a6
Raphael <rhts478@gmail.com>
2026-05-21 13:19:00 -0300

    fix(UI): resolvido inconsistência visual na página de Instituições Cadastradas. Problema: a listbox era incosistente com o padrão abordado pelo sistema definido na página de Docentes Cadastrados (pessoaList.zul).
    Foram feitas correções no padding e utilizada classes Bootstrap de fixação de tamanho (h-100).

--- src/main/java/com/uem/extrator/viewmodel/ListagemVM.java
+++ src/main/java/com/uem/extrator/viewmodel/ListagemVM.java
@@ -38,7 +38,7 @@ public class ListagemVM {
 //    private List<Curriculo> listaCurriculosCompleta = new ArrayList<>(); // cache para nao ir ao banco toda vez
 //    private List<Curriculo> listaCurriculos = new ArrayList<>(); // lista que aparece na tela
     private List<Curriculo> listaCurriculos = new ArrayList<>(); // apenas os (tamanhoPagina) da tela atual
-    private int tamanhoPagina = 18;
+    private int tamanhoPagina = 21;
     private int paginaAtual = 0;
     private long totalCurriculos = 0;
 
